779ae5d73e9547da916facdb6ea08bb811288f9b
adiep2 <diep@ualberta.ca>
2022-04-05 16:41:21 -0600

    Initial database connection WIP

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,11 +1,45 @@
 #[macro_use] extern crate rocket;
+use mongodb::{options::ClientOptions, sync::Client};
 
 #[get("/hello")]
 fn index() -> &'static str {
     "Hello, world!"
 }
 
-#[launch]
-fn rocket() -> _ {
-    rocket::build().mount("/", routes![index])
+fn rocket() -> Result<rocket::Rocket<rocket::Build>, mongodb::error::Error> {
+    // match ClientOptions::parse(
+    //     "mongodb+srv://myUser:myPassword@mycluster.zvnqo.mongodb.net/MyCluster?retryWrites=true&w=majority",
+    // ){
+    //     Some(client_options) => {
+    //         let client = Client::with_options(client_options)?;
+    //         let database = client.database("Connect4DB");
+        
+    //         Ok(rocket::build().mount("/", routes![index]))
+    //     }
+    //     Err(error) => {
+    //         Err(error)
+    //     }
+    // };
+
+    let client_options = ClientOptions::parse(
+        "mongodb+srv://myUser:myPassword@mycluster.zvnqo.mongodb.net/MyCluster?retryWrites=true&w=majority",
+    )?;
+    let client = Client::with_options(client_options)?;
+    for db_name in client.list_database_names(None, None)? {
+        println!("{}", db_name);
+    }
+    // let database = client.database("Connect4DB");
+    Ok(rocket::build().mount("/", routes![index]))
+}
+
+#[rocket::main]
+async fn main() {
+    // launch server or report error
+    match rocket() {
+        Ok(rocket) => {
+            let error = rocket.launch().await;
+            // eprintln!("Failed to launch server: {}", error);
+        }
+        Err(error) => eprintln!("Failed to create server: {}", error),
+    }
 }
