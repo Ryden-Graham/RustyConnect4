42a307b8e798efe8a75914e42a1f5dfc79e2631b
ccchoi2 <ccchoi@ualberta.ca>
2022-04-07 10:36:08 -0600

    new sidebar stuff

--- frontend/src/home.rs
+++ frontend/src/home.rs
@@ -11,13 +11,20 @@ pub fn home() -> Html {
 
     html! {
             <div class="w3-container" id="services" style="margin-top:75px">
-                <h5 class="w3-xxxlarge w3-text-red"><b>{"How to Play Connect 4"}</b></h5>
-                <hr style="width:50px;border:5px solid red" class="w3-round"/>
+                <h5 class="bodyHeader"><b>{"How to Play Connect 4"}</b></h5>
+                <hr style="width:50px;border:5px solid red" class="headerDivider"/>
+
+                <div>
                 <p>
                     {"Connect Four is a two-player connection game in which the players take turns dropping colored discs from the top into a seven-column, six-row vertically suspended grid. The objective of the game is to be the first to form a horizontal, vertical, or diagonal line of four of one's own discs."}
                 </p>
+                </div>
+
                 <br/>
+
                 <div><h5>{"To play Connect 4 follow the following steps:"}</h5></div>
+
+                <div>
                 <ul>
 
                     <li>{"A new game describes discs of which color belongs to which player"}</li>
@@ -27,10 +34,16 @@ pub fn home() -> Html {
                     <li>{"Try to connect 4 of your colored discs either horizontally or vertically or diagonally"}</li>
 
                 </ul>
+                </div>
+
                 <br/>
+
+                <div>
                 <p>
                     {"For More information on Connect 4 click "}<a href="https://en.wikipedia.org/wiki/Connect_Four">{"here"}</a>
                 </p>
+                </div>
+                
             </div>
         }
 }
--- frontend/src/sidebar.rs
+++ frontend/src/sidebar.rs
@@ -16,7 +16,7 @@ enum AppRoute {
 pub enum Msg {
     SwitchHome,
 }
-pub struct Header;
+pub struct Sidebar;
 
 #[derive(Clone, PartialEq, Properties)]
 pub struct Props {
@@ -25,7 +25,7 @@ pub struct Props {
 }
 
 
-impl Component for Header {
+impl Component for Sidebar {
     type Message = Msg;
     type Properties = Props;
 
@@ -47,23 +47,46 @@ impl Component for Header {
     fn view(&self, ctx: &Context<Self>) -> Html {
         // This gives us a component's "`Scope`" which allows us to send messages, etc to the component.
         html! {
-            <div class="header">
-                <div class="header-logo-link" onclick={ctx.link().callback(|_| Msg::SwitchHome)}>
-                    <img class="header-logo" src="img/rust.png" />
-                    <p class="header-logo-text">{ ctx.props().title.clone() }</p>
+            <>
+                <nav class="w3-sidenav w3-red w3-collapse w3-top w3-large w3-padding" style="z-index:3;width:350px;font-weight:bold" id="mySidenav">
+                    <a href="javascript:void(0)" class="w3-padding-xlarge w3-hide-large w3-display-topleft w3-hover-white" style="width:100%">{"Close Menu"}</a>
+                    <div class="w3-container">
+                        <h3 class="w3-padding-64"><b>{"Play"}<br/> {"Connect4 / TOOT-OTTO"}</b></h3>
+                    </div>
+
+                    // {for list_items}
+                </nav>
+                <header class="w3-container w3-top w3-hide-large w3-red w3-xlarge w3-padding">
+                <a href="javascript:void(0)" class="w3-btn w3-red w3-border w3-border-white w3-margin-right">{"\u{2630}"}</a>
+                <span>{"Connect 4 with MEAN"}</span>
+                </header>
+                <div class="w3-overlay w3-hide-large" style="cursor:pointer" title="close side menu" id="myOverlay"></div>
+                <div class="w3-main" style="margin-left:390px;margin-right:40px">
+                // {
+                    // html !{
+                    //     <DisplayWindow uri={active_markdown_uri} />
+                    // }
+                // }
                 </div>
-                <div class="header-pages">
-                    <a class="header-page-link" href = "/">
-                        <p class="header-page-text">{ "Home" }</p>
-                    </a>
-                    <a class="header-page-link" href = "/connect4">
-                        <p class="header-page-text">{ "Connect4" }</p>
-                    </a>
-                </div>
-            </div>
+            </>
         }
     }
 }
+
+// <div class="header">
+//     <Link<AppRoute> classes={classes!("header-logo-link")} to={AppRoute::Home}>
+//         <img class="header-logo" src="img/rust.png" />
+//         <p class="header-logo-text">{ "Rusty Connect 4" }</p>
+//     </Link<AppRoute>>
+//     <div class="header-pages">
+//         <Link<AppRoute> classes={classes!("header-page-link")} to={AppRoute::Home}>
+//             <p class="header-page-text">{ "Home" }</p>
+//         </Link<AppRoute>>
+//         <Link<AppRoute> classes={classes!("header-page-link")} to={AppRoute::Connect4}>
+//             <p class="header-page-text">{ "Connect4" }</p>
+//         </Link<AppRoute>>
+//     </div>
+// </div>
 
 // use std::borrow::Borrow;
 // use yew_router::prelude::*;
